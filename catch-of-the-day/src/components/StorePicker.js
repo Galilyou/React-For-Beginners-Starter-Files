b685278cfb3045a229cee9f8e7c6d55888b2726a
Ahmed Elhalwagy <ahmed.elhalwagy@nagwa.com>
2018-07-24 00:09:29 +0200

    explains how to bind react's 'this' to methods that are not in the react lifecylc via constructor

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,42 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // surfacing an input from a form to its react-based form submit handler
+  // this can be done in several ways, worst of all is directly accessing the DOM
+  // using document.querySelector() or similar function.
+  // right here: we will do it using refs in react.
+
+  storeNameInput = React.createRef();
+
+  // read about when functions are bound (or mounted) in the react lifecycle.
+  // in short, 'this' will not be bound in each method, sometimes you will need
+  // bind it yourself int he constructor, like below.
+
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  goToStore(event) {
+    // STOP FORM SUBMIT TO SERVER
+    event.preventDefault();
+
+    console.log(this.storeNameInput.current.value);
+  }
+
   render() {
+    console.log(this);
     return (
-      <form action="post" className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          ref={this.storeNameInput}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
