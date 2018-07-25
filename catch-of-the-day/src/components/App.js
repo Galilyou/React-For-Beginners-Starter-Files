2c7971b278f73dd4a2d58811ad7128fe5d1b11a6
Ahmed Elhalwagy <ahmed.elhalwagy@nagwa.com>
2018-07-25 22:53:29 +0200

    Calculates total price of all order and displays that total price; only that total price.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -34,6 +34,17 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = key => {
+    // 1. get a copy of state.order
+    const order = { ...this.state.order };
+    // 2. add the new key that was sent to it
+    order[key] = order[key] + 1 || 1;
+    // 3. setState with the new copy
+    this.setState({
+      order: order
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -41,11 +52,16 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
