24759fee8f964ad724d5af466825cafb58b79f07
Ahmed Elhalwagy <ahmed.elhalwagy@nagwa.com>
2018-07-25 23:05:32 +0200

    renders the order pane properly .. next up persisting state.

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,6 +2,27 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === "available";
+
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
+    }
+
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
   render() {
     const orderIds = Object.keys(this.props.order);
 
@@ -19,9 +40,9 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Orders</h2>
-        {orderIds}
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
         <div className="total">
-          <strong>{formatPrice(total)}</strong>
+          Total: <strong>{formatPrice(total)}</strong>
         </div>
       </div>
     );
